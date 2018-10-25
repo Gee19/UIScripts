a5e447669a98989f2949b61305e10310ade1fc46
Jordan <Gee19@users.noreply.github.com>
2018-10-24 22:50:47 -0400

    frames top left

--- UIScripts.lua
+++ UIScripts.lua
@@ -1,5 +1,27 @@
 -- This file is loaded from "UIScripts.toc"
+-- http://www.arenajunkies.com/topic/222642-default-ui-scripts/
 
+-- MainMenuBarArtFrame.LeftEndCap:Hide()
+-- MainMenuBarArtFrame.RightEndCap:Hide()
+-- StatusTrackingBarManager:Hide()
+-- ObjectiveTrackerFrame:Hide()
+
+FramerateLabel:ClearAllPoints()
+FramerateText:ClearAllPoints()
+FramerateLabel:SetPoint("RIGHT",UIParent,"CENTER",-250,0)
+FramerateText:SetPoint("LEFT",FramerateLabel,"RIGHT")
+
+MainMenuBarArtFrameBackground:Hide()
+MainMenuBarArtFrame.PageNumber:Hide()
+ActionBarUpButton:SetAlpha(0)
+ActionBarDownButton:SetAlpha(0)
+
+StatusTrackingBarManager:Hide();
+MainMenuBarArtFrame.RightEndCap:Hide();
+MainMenuBarArtFrame.LeftEndCap:Hide();
+
+MainMenuBarArtFrameBackground:ClearAllPoints() 
+MainMenuBarArtFrameBackground:SetPoint("CENTER",23,-23) MainMenuBarArtFrameBackground.SetPoint = function() end
 
 -- Hide Target Castbar
 -- SetCVar("showTargetCastbar", 0)
@@ -9,42 +31,89 @@
 -- CastingBarFrame:SetPoint("TOP", WorldFrame, "CENTER", 0, -350)
 -- CastingBarFrame.SetPoint = function() end
 
+------------------------------------------------
 -- Dispellable debuffs
+------------------------------------------------
 -- hooksecurefunc("TargetFrame_UpdateAuras", function(s) for i=1,MAX_TARGET_BUFFS do if select(5,UnitAura(s.unit,i)) == 'Magic' and UnitIsEnemy('player',s.unit) then _G[s:GetName().."Buff"..(i).."Stealable"]:Show() end end end)
 
+------------------------------------------------
+-- /afk surrender
+------------------------------------------------
+local function prettyPrint(msg)
+    print("|cFFDC143CafkSurrender:|r |cFF40E0D0"..msg.."|r")
+end
+
+SlashCmdList["CHAT_AFK"] = function(msg)
+    if IsActiveBattlefieldArena() then
+        if CanSurrenderArena() then
+            prettyPrint("Successfully surrendered arena.")
+            SurrenderArena();
+        else
+            prettyPrint("Failed to surrender arena. Partners still alive.")
+        end
+    else
+        SendChatMessage(msg, "AFK");
+    end
+end
+
+------------------------------------------------
+-- Minimap Tweaks
+------------------------------------------------
+MinimapZoomIn:Hide()
+MinimapZoomOut:Hide()
+Minimap:EnableMouseWheel(true)
+Minimap:SetScript('OnMouseWheel', function(self, delta)
+    if delta > 0 then
+        Minimap_ZoomIn()
+    else
+        Minimap_ZoomOut()
+    end
+end)
+MiniMapTracking:ClearAllPoints()
+MiniMapTracking:SetPoint("TOPRIGHT", -26, 7)
 
 -- Move Tooltip
--- GameTooltip:SetScript("OnTooltipSetUnit", function(self)
---     self:ClearAllPoints()
---     self:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", -30, 200)
--- end)
+GameTooltip:SetScript("OnTooltipSetUnit", function(self)
+    self:ClearAllPoints()
+    self:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", -20, 135)
+end)
 
+-- Focus Cast Bar
+hooksecurefunc(FocusFrameSpellBar, "Show", function()
+    FocusFrameSpellBar:ClearAllPoints()
+    FocusFrameSpellBar:SetPoint("CENTER", UIParent, "CENTER", 25, -100)
+    FocusFrameSpellBar:SetScale(1.5)
+    FocusFrameSpellBar.SetPoint = function() end
+end)
 
-------------------------------------------------
--- Penance Aura
-------------------------------------------------
--- local frame = CreateFrame("FRAME")
--- frame:RegisterEvent("UNIT_AURA")
-
--- frame:SetScript("OnEvent", function(self, event, ...)
---     local unitid = ... if unitid ~= "player" then return end
-
---     if UnitBuff("player", "Power of the Dark Side") then
---         SpellActivationOverlay_ShowOverlay(SpellActivationOverlayFrame, 198068, "TEXTURES\\SPELLACTIVATIONOVERLAYS\\GENERICTOP_01.BLP", "TOP", 1.2, 139, 65, 239, false, false)
---     else
---         SpellActivationOverlay_HideOverlays(SpellActivationOverlayFrame, 198068)
---     end
--- end)
-
+-- Target Cast Bar
+hooksecurefunc(CastingBarFrame, "Show", function()
+    CastingBarFrame:ClearAllPoints()
+    CastingBarFrame:SetPoint("CENTER", UIParent, "CENTER", 0, -310)
+    CastingBarFrame.SetPoint = function() end
+end)
 
 ------------------------------------------------
 -- Move buffs & debuffs
 ------------------------------------------------
-function Movebuff() BuffFrame:ClearAllPoints() BuffFrame:SetScale(1.1) BuffFrame:SetPoint("CENTER",PlayerFrame,"CENTER",550,500) end  hooksecurefunc("UIParent_UpdateTopFramePositions",Movebuff) Movebuff()
+function Movebuff() BuffFrame:ClearAllPoints() BuffFrame:SetScale(1.1) BuffFrame:SetPoint("CENTER",PlayerFrame,"CENTER",760,100) end  hooksecurefunc("UIParent_UpdateTopFramePositions",Movebuff) Movebuff()
 
 function sp(f,i) tr="TOPRIGHT";f2=f.debuffFrames;s=f2[1]:GetWidth();f3=f2[i];f3:SetSize(s,s);f3:ClearAllPoints();if i>6 then f3:SetPoint("BOTTOMRIGHT",f2[i-3],tr,0,0) else f3:SetPoint(tr,f2[1],tr,-(s*(i-3)),0) end end
 function CBF(f,i) bf=CreateFrame("Button",f:GetName().."Debuff"..i,f,"CompactDebuffTemplate");bf.baseSize=22;bf:SetSize(f.buffFrames[1]:GetSize()) end;function mv(f) for i=4,12 do sp(f,i) end end
 function mv3(f) CompactUnitFrame_SetMaxDebuffs(f,12); if not f.debuffFrames[4] then for i=4,12 do CBF(f,i) end end mv(f) end;hooksecurefunc("CompactUnitFrame_UpdateDebuffs",function(f) if f:GetName():match("^Compact") then mv3(f) end end);
+
+-- Hide Talking Head
+local function HookTalkingHead()
+    hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+        TalkingHeadFrame:Hide()
+    end)
+end
+ 
+if TalkingHeadFrame then
+    HookTalkingHead()
+else -- Hook to the loading function.
+    hooksecurefunc('TalkingHead_LoadUI', HookTalkingHead)
+end
 
 
 ------------------------------------------------
@@ -63,6 +132,22 @@ hooksecurefunc("UnitFramePortrait_Update",function(self)
                 end
         end
 end)
+
+------------------------------------------------
+-- Penance Aura
+------------------------------------------------
+-- local frame = CreateFrame("FRAME")
+-- frame:RegisterEvent("UNIT_AURA")
+
+-- frame:SetScript("OnEvent", function(self, event, ...)
+--     local unitid = ... if unitid ~= "player" then return end
+
+--     if UnitBuff("player", "Power of the Dark Side") then
+--         SpellActivationOverlay_ShowOverlay(SpellActivationOverlayFrame, 198068, "TEXTURES\\SPELLACTIVATIONOVERLAYS\\GENERICTOP_01.BLP", "TOP", 1.2, 139, 65, 239, false, false)
+--     else
+--         SpellActivationOverlay_HideOverlays(SpellActivationOverlayFrame, 198068)
+--     end
+-- end)
 
 ------------------------------------------------
 -- ColorFrames
@@ -136,51 +221,51 @@ end)
 ------------------------------------------------
 -- ClassColoredHealthBars
 ------------------------------------------------
-local function removeAlphaFrom(f)
-    f:SetAlpha(0)
-    f.SetAlpha = function() end
-end
-local usc = UnitSelectionColor
-function UnitSelectionColor(u, ...)
-    if u == TargetFrame.unit or u == FocusFrame.unit then
-        return 0, 0, 0, 0.4;
-    end
-    r, g, b, a = usc(u, ...)
-    return r, g, b, a;
-end
-removeAlphaFrom(PlayerPrestigeBadge);
-removeAlphaFrom(PlayerPrestigePortrait);
-removeAlphaFrom(TargetFrameTextureFramePrestigeBadge);
-removeAlphaFrom(TargetFrameTextureFramePrestigePortrait);
-removeAlphaFrom(FocusFrameTextureFramePrestigeBadge);
-removeAlphaFrom(FocusFrameTextureFramePrestigePortrait);
-PlayerName:SetTextColor(1,1,1,1);
-TargetFrameTextureFrameName:SetTextColor(1,1,1,1);
-FocusFrameTextureFrameName:SetTextColor(1,1,1,1);
+-- local function removeAlphaFrom(f)
+--     f:SetAlpha(0)
+--     f.SetAlpha = function() end
+-- end
+-- local usc = UnitSelectionColor
+-- function UnitSelectionColor(u, ...)
+--     if u == TargetFrame.unit or u == FocusFrame.unit then
+--         return 0, 0, 0, 0.4;
+--     end
+--     r, g, b, a = usc(u, ...)
+--     return r, g, b, a;
+-- end
+-- removeAlphaFrom(PlayerPrestigeBadge);
+-- removeAlphaFrom(PlayerPrestigePortrait);
+-- removeAlphaFrom(TargetFrameTextureFramePrestigeBadge);
+-- removeAlphaFrom(TargetFrameTextureFramePrestigePortrait);
+-- removeAlphaFrom(FocusFrameTextureFramePrestigeBadge);
+-- removeAlphaFrom(FocusFrameTextureFramePrestigePortrait);
+-- PlayerName:SetTextColor(1,1,1,1);
+-- TargetFrameTextureFrameName:SetTextColor(1,1,1,1);
+-- FocusFrameTextureFrameName:SetTextColor(1,1,1,1);
 
-local   UnitIsPlayer, UnitIsConnected, UnitClass, RAID_CLASS_COLORS =
-        UnitIsPlayer, UnitIsConnected, UnitClass, RAID_CLASS_COLORS
-local _, class, c
+-- local   UnitIsPlayer, UnitIsConnected, UnitClass, RAID_CLASS_COLORS =
+--         UnitIsPlayer, UnitIsConnected, UnitClass, RAID_CLASS_COLORS
+-- local _, class, c
 
-local function colour(statusbar, unit)
-    if UnitIsPlayer(unit) and UnitIsConnected(unit) and unit == statusbar.unit and UnitClass(unit) then
-        _, class = UnitClass(unit)
-        c = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
-        statusbar:SetStatusBarColor(c.r, c.g, c.b)
-    end
-end
+-- local function colour(statusbar, unit)
+--     if UnitIsPlayer(unit) and UnitIsConnected(unit) and unit == statusbar.unit and UnitClass(unit) then
+--         _, class = UnitClass(unit)
+--         c = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
+--         statusbar:SetStatusBarColor(c.r, c.g, c.b)
+--     end
+-- end
 
-hooksecurefunc("UnitFrameHealthBar_Update", colour)
-hooksecurefunc("HealthBar_OnValueChanged", function(self)
-    colour(self, self.unit)
-end)
+-- hooksecurefunc("UnitFrameHealthBar_Update", colour)
+-- hooksecurefunc("HealthBar_OnValueChanged", function(self)
+--     colour(self, self.unit)
+-- end)
 
-local sb = _G.GameTooltipStatusBar
-local addon = CreateFrame("Frame", "StatusColour")
-addon:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-addon:SetScript("OnEvent", function()
-    colour(sb, "mouseover")
-end)
+-- local sb = _G.GameTooltipStatusBar
+-- local addon = CreateFrame("Frame", "StatusColour")
+-- addon:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+-- addon:SetScript("OnEvent", function()
+--     colour(sb, "mouseover")
+-- end)
 
 
 ------------------------------------------------
@@ -259,101 +344,3 @@ end)
 --         end
 --     end
 -- end
-
-
-------------------------------------------------
--- SafeQueue
-------------------------------------------------
--- local SafeQueue = CreateFrame("Frame")
--- local queueTime
--- local queue = 0
--- local remaining = 0
--- SafeQueueDB = SafeQueueDB or { announce = "self" }
-
--- PVPReadyDialog.leaveButton:Hide()
--- PVPReadyDialog.leaveButton.Show = function() end -- Prevent other mods from showing the button
--- PVPReadyDialog.enterButton:ClearAllPoints()
--- PVPReadyDialog.enterButton:SetPoint("BOTTOM", PVPReadyDialog, "BOTTOM", 0, 25)
--- PVPReadyDialog.label:SetPoint("TOP", 0, -22)
-
--- local function Print(msg)
---     DEFAULT_CHAT_FRAME:AddMessage("|cff33ff99SafeQueue|r: " .. msg)
--- end
-
--- local function PrintTime()
---     local announce = SafeQueueDB.announce
---     if announce == "off" then return end
---     local secs, str, mins = floor(GetTime() - queueTime), "Queue popped "
---     if secs < 1 then
---         str = str .. "instantly!"
---     else
---         str = str .. "after "
---         if secs >= 60 then
---             mins = floor(secs/60)
---             str = str .. mins .. "m "
---             secs = secs%60
---         end
---         if secs%60 ~= 0 then
---             str = str .. secs .. "s"
---         end
---     end
---     if announce == "self" or not IsInGroup() then
---         Print(str)
---     else
---         local group = IsInRaid() and "RAID" or "PARTY"
---         SendChatMessage(str, group)
---     end
--- end
-
--- SafeQueue:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
--- SafeQueue:SetScript("OnEvent", function()
---     local queued
---     for i=1, GetMaxBattlefieldID() do
---         local status = GetBattlefieldStatus(i)
---         if status == "queued" then
---             queued = true
---             if not queueTime then queueTime = GetTime() end
---         elseif status == "confirm" then
---             if queueTime then
---                 PrintTime()
---                 queueTime = nil
---                 remaining = 0
---                 queue = i
---             end                 
---         end
---         break
---     end
---     if not queued and queueTime then queueTime = nil end
--- end)
-
--- SafeQueue:SetScript("OnUpdate", function(self)
---     if PVPReadyDialog_Showing(queue) then
---         local secs = GetBattlefieldPortExpiration(queue)
---         if secs and secs > 0 and remaining ~= secs then
---             remaining = secs
---             local color = secs > 20 and "f20ff20" or secs > 10 and "fffff00" or "fff0000"
---             PVPReadyDialog.label:SetText("Expires in |cf"..color.. SecondsToTime(secs) .. "|r")
---         end
---     end
--- end)
-
--- SlashCmdList.SafeQueue = function(msg)
---     msg = msg or ""
---     local cmd, arg = string.split(" ", msg, 2)
---     cmd = string.lower(cmd or "")
---     arg = string.lower(arg or "")
---     if cmd == "announce" then
---         if arg == "off" or arg == "self" or arg == "group" then
---             SafeQueueDB.announce = arg
---             Print("Announce set to " .. arg)
---         else
---             Print("Announce set to " .. SafeQueueDB.announce)
---             Print("Announce types are \"off\", \"self\", and \"group\"")
---         end
---     else
---         DEFAULT_CHAT_FRAME:AddMessage("|cff33ff99SafeQueue v2.7|r")
---         Print("/sq announce : " .. SafeQueueDB.announce)
---     end
--- end
--- SLASH_SafeQueue1 = "/safequeue"
--- SLASH_SafeQueue2 = "/sq"
